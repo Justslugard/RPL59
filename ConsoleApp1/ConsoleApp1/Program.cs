4b45d0fb58743c0330f38160ac61a9a5b4418b77
Justslugard <bot3445x@gmail.com>
2025-08-07 07:00:17 +0700

    tweaking c#

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,22 +11,22 @@ namespace MyFirstApp
         static void Main(string[] args)
         {
             DateTime now = DateTime.Now;
+            int thisYear = Convert.ToInt32(now.ToString("yyyy"));
 
             string fName = "Nizam", mName = "Rizki", lName = "Syahputra";
             string address = "Komplek Essence Park Blok A7 No 7 RT 001 RW 013 Jl. Masjid Raya Jati Cempaka Pondok Gede";
             string bornPlace = "Jakarta", bornMonth = "April";
-            int bornYear = 2009;
-            int bornDate = 17;
-            //int umur = 
+            int bornYear = 2009, bornDate = 17;
 
             // Ts with $
             Console.WriteLine("Memakai $ atau \"String Interpolation\"\n");
             Console.WriteLine($"Nama:\n{fName} {mName} {lName}\n");
             Console.WriteLine($"Alamat:\n{address}\n");
             Console.WriteLine($"Tempat / Tanggal Lahir:\n{bornPlace}, {bornDate} {bornMonth} {bornYear}\n");
-            Console.WriteLine($"Umur:\n{Convert.ToInt32(now.ToString("yyyy")) - bornYear} Tahun\n");
+            Console.WriteLine($"Umur:\n{thisYear - bornYear} Tahun\n");
 
             Console.WriteLine("===================");
+
             // Ts with +
             Console.WriteLine("\nMemakai + atau \"String Concatenation\"\n");
             Console.WriteLine("Nama:\n" + fName + " " + mName + " " + lName);
@@ -35,7 +35,7 @@ namespace MyFirstApp
             Console.WriteLine();
             Console.WriteLine("Tempat / Tanggal Lahir:\n" + bornPlace + ", " + bornDate + " " + bornMonth + " " + bornYear);
             Console.WriteLine();
-            Console.WriteLine("Umur:\n" + (Convert.ToInt32(now.ToString("yyyy")) - bornYear) + " tahun");
+            Console.WriteLine("Umur:\n" + (thisYear - bornYear) + " tahun");
 
             Console.ReadKey();
         }
