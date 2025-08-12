1332747b76451d4a85318a8403a194f74c10580d
Justslugard <bot3445x@gmail.com>
2025-08-13 06:57:02 +0700

    Console login

---
+++ Csharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biodata
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            //DateTime now = DateTime.Today;
+            //DateTime born = new DateTime(2009, 04, 17);
+
+            //int diffYear = now.Year - born.Year, diffMonth = now.Month - born.Month, diffDate = now.Day - born.Day;
+
+            //Console.WriteLine($"{diffYear} {diffMonth} {diffDate}");
+
+            //if (diffDate < 0)
+            //{
+
+            //}
+
+            //string fName = "Nizam", mName = "Rizki", lName = "Syahputra";
+            //string address = "Komplek Essence Park Blok A7 No 7 RT 001 RW 013 Jl. Masjid Raya Jati Cempaka Pondok Gede";
+            //string bornPlace = "Jakarta", bornMonth = "April";
+            //int bornYear = 2009, bornDate = 17;
+
+            //// Ts with $
+            //Console.WriteLine("Memakai $ atau \"String Interpolation\"\n");
+            //Console.WriteLine($"Nama:\n{fName} {mName} {lName}\n");
+            //Console.WriteLine($"Alamat:\n{address}\n");
+            //Console.WriteLine($"Tempat / Tanggal Lahir:\n{bornPlace}, {bornDate} {bornMonth} {bornYear}\n");
+            ////Console.WriteLine($"Umur:\n{tsYear - bornYear} Tahun\n");
+
+            //Console.WriteLine("===================");
+
+            //// Ts with +
+            //Console.WriteLine("\nMemakai + atau \"String Concatenation\"\n");
+            //Console.WriteLine("Nama:\n" + fName + " " + mName + " " + lName);
+            //Console.WriteLine();
+            //Console.WriteLine("Alamat:\n" + address);
+            //Console.WriteLine();
+            //Console.WriteLine("Tempat / Tanggal Lahir:\n" + bornPlace + ", " + bornDate + " " + bornMonth + " " + bornYear);
+            //Console.WriteLine();
+            ////Console.WriteLine("Umur:\n" + (tsYear - bornYear) + " tahun");
+
+            int x = 0b0110;
+            int y = 0b0011;
+            int res = x ^= y;
+            Console.WriteLine(Convert.ToString(res, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp/Loginkan/Loginkan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loginkan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                Console.Write("Siapa namamu tuan? ");
+                string log = Console.ReadLine();
+                Console.Write("Kata kuncinya tuan? ");
+                string pw = Console.ReadLine();
+                if (log == "wiwokdetok" && pw == "admin#123") 
+                {
+                    Console.WriteLine("Hidoep Jokowi");
+                    break;
+                }
+                Console.WriteLine("Salah Njir\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
