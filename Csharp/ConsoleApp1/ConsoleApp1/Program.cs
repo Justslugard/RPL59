ca8e083f0f8bb205c975198b546df6a681395b79
Justslugard <bot3445x@gmail.com>
2025-08-16 07:59:41 +0700

    css yay

--- Csharp/ConsoleApp1/ConsoleApp1/Program.cs
+++ Csharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,46 +11,41 @@ namespace Biodata
     {
         static void Main(string[] args)
         {
-            //DateTime now = DateTime.Today;
-            //DateTime born = new DateTime(2009, 04, 17);
+            DateTime now = DateTime.Today;
+            DateTime born = new DateTime(2009, 04, 17);
 
-            //int diffYear = now.Year - born.Year, diffMonth = now.Month - born.Month, diffDate = now.Day - born.Day;
+            int diffYear = now.Year - born.Year, diffMonth = now.Month - born.Month, diffDate = now.Day - born.Day;
 
-            //Console.WriteLine($"{diffYear} {diffMonth} {diffDate}");
+            Console.WriteLine($"{diffYear} {diffMonth} {diffDate}");
 
-            //if (diffDate < 0)
-            //{
+            if (diffDate < 0)
+            {
 
-            //}
+            }
 
-            //string fName = "Nizam", mName = "Rizki", lName = "Syahputra";
-            //string address = "Komplek Essence Park Blok A7 No 7 RT 001 RW 013 Jl. Masjid Raya Jati Cempaka Pondok Gede";
-            //string bornPlace = "Jakarta", bornMonth = "April";
-            //int bornYear = 2009, bornDate = 17;
+            string fName = "Nizam", mName = "Rizki", lName = "Syahputra";
+            string address = "Komplek Essence Park Blok A7 No 7 RT 001 RW 013 Jl. Masjid Raya Jati Cempaka Pondok Gede";
+            string bornPlace = "Jakarta", bornMonth = "April";
+            int bornYear = 2009, bornDate = 17;
 
-            //// Ts with $
-            //Console.WriteLine("Memakai $ atau \"String Interpolation\"\n");
-            //Console.WriteLine($"Nama:\n{fName} {mName} {lName}\n");
-            //Console.WriteLine($"Alamat:\n{address}\n");
-            //Console.WriteLine($"Tempat / Tanggal Lahir:\n{bornPlace}, {bornDate} {bornMonth} {bornYear}\n");
-            ////Console.WriteLine($"Umur:\n{tsYear - bornYear} Tahun\n");
+            // Ts with $
+            Console.WriteLine("Memakai $ atau \"String Interpolation\"\n");
+            Console.WriteLine($"Nama:\n{fName} {mName} {lName}\n");
+            Console.WriteLine($"Alamat:\n{address}\n");
+            Console.WriteLine($"Tempat / Tanggal Lahir:\n{bornPlace}, {bornDate} {bornMonth} {bornYear}\n");
+            //Console.WriteLine($"Umur:\n{tsYear - bornYear} Tahun\n");
 
-            //Console.WriteLine("===================");
+            Console.WriteLine("===================");
 
-            //// Ts with +
-            //Console.WriteLine("\nMemakai + atau \"String Concatenation\"\n");
-            //Console.WriteLine("Nama:\n" + fName + " " + mName + " " + lName);
-            //Console.WriteLine();
-            //Console.WriteLine("Alamat:\n" + address);
-            //Console.WriteLine();
-            //Console.WriteLine("Tempat / Tanggal Lahir:\n" + bornPlace + ", " + bornDate + " " + bornMonth + " " + bornYear);
-            //Console.WriteLine();
-            ////Console.WriteLine("Umur:\n" + (tsYear - bornYear) + " tahun");
-
-            int x = 0b0110;
-            int y = 0b0011;
-            int res = x ^= y;
-            Console.WriteLine(Convert.ToString(res, 2));
+            // Ts with +
+            Console.WriteLine("\nMemakai + atau \"String Concatenation\"\n");
+            Console.WriteLine("Nama:\n" + fName + " " + mName + " " + lName);
+            Console.WriteLine();
+            Console.WriteLine("Alamat:\n" + address);
+            Console.WriteLine();
+            Console.WriteLine("Tempat / Tanggal Lahir:\n" + bornPlace + ", " + bornDate + " " + bornMonth + " " + bornYear);
+            Console.WriteLine();
+            //Console.WriteLine("Umur:\n" + (tsYear - bornYear) + " tahun");
 
             Console.ReadKey();
         }
